d9fcec1cd1d87708a926a44824bc6193cadef41c
Dmitry Shevchenok <dm.shevchenok@gmail.com>
2020-09-28 16:41:57 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined||array.length === 0 ) {
+    return 0;
+  }
+  let min = array[0]
+  array.forEach((item)=>{
+    if (min > item) {
+      min = item;
+    }
+  })
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined||array.length === 0 ) {
+    return 0;
+  }
+  let max = array[0]
+  array.forEach((item)=>{
+    if (max < item) {
+      max = item;
+    }
+  })
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined||array.length === 0 ) {
+    return 0;
+  }
+let avg ;
+let newArr = array.reduce((prev, curr)=> prev+ curr)
+ return avg = newArr/array.length
 }
